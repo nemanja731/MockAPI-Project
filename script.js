5f5bb92e9e6de6b15e6c8a2c48669918bc50d36f
Nemanja Jankovic <nemanja.jankovic731@gmail.com>
2022-11-15 15:06:20 +0100

    Update label

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.querySelector('button').addEventListener('click', e =>  {
   let r = fetch('https://637388cf348e9472990f4755.mockapi.io/users/' + id)
   .then(response => response.json()).then(data => {
     console.log(data);
-    let podaci = document.querySelector('.data');
-    podaci.innerHTML = `<h2>User with id ${id}:</h2>
+    let showData = document.querySelector('.data');
+    showData.innerHTML = `<h2>User with id ${id}:</h2>
                       <p><b>Username:</b> ${data['username']}</p>
                       <p><b>Email:</b> ${data['email']}</p>
                       <p><b>Password:</b> ${data['password']}</p>`;
